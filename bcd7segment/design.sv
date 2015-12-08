868f8dc21caf49777d699bbfc1bbba837eb7112a
mdrush <mrusheen@gmail.com>
2015-12-07 20:32:41 -0800

    Fixed input

--- bcd7segment/design.sv
+++ bcd7segment/design.sv
@@ -1,13 +1,13 @@
 /*
  * BCD to 7-segment display decoder
  */ 
-module sevenseg(clk, a, out);
+module sevenseg(clk, in, out);
   input  clk;
   input  [3:0] in;
   output reg [6:0] out; //abcdefg
   
   always @(posedge clk) begin
-    case (a)
+    case (in)
       4'b0000: out = 7'b1111110;
       4'b0001: out = 7'b0110000;
       4'b0010: out = 7'b1101101;
